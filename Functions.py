d95044070749d1745b74c4e5832eef421ef2c4eb
Elena Goryacheva <kaeru.geo@gmail.com>
2016-12-01 12:43:51 +0300

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,7 +1,7 @@
 """Functions - zip(), list comprehensions."""
 
 
-def zip_list(args):
+def zip_list(*args):
     """Do the same as function zip() does."""
     zipped_lists = []
     min_len = min(len(arg) for arg in args)
@@ -29,15 +29,34 @@ def super_squares(my_list):
 
 
 print "Firstly, look on these list comprehensions. Input a list:"
-X = map(int, raw_input().split())
+while 1:
+    try:
+        X = map(int, raw_input().split())
+        break
+    except ValueError as err:
+        print 'Please insert only numbers. ({})'.format(err)
 print "Squares: \t", squares(X)
 print "Each second elem: \t", seconds(X)
 print "Squares of even elem in odd positions: \t", super_squares(X)
 print "-"*20
-print "We also have a zip() function. How many lists do you want to zip?"
+print "Try zip() function. How many lists do you want to zip? (min = 2)"
 LISTS_TO_ZIP = []
-for i in range(int(raw_input())):
-    print "Ok, insert list #", str(i)+":"
-    Y = map(int, raw_input().split())
+while 1:
+    try:
+        N = int(raw_input())
+        if N > 1:
+            break
+        else:
+            print "Zip function works for 2 or more lists. Try again"
+    except ValueError as err:
+        print 'Please insert only numbers. ({})'.format(err)
+for i in range(N):
+    print "Ok, insert list #{}:".format(i)
+    while 1:
+        try:
+            Y = map(int, raw_input().split())
+            break
+        except ValueError as err:
+            print 'Please insert only numbers. ({})'.format(err)
     LISTS_TO_ZIP.append(Y)
-print zip_list(LISTS_TO_ZIP)
+print zip_list(*LISTS_TO_ZIP)
