b6c51e2c1e810da18ea680c429b193a78f655fbb
Elena Goryacheva <kaeru.geo@gmail.com>
2016-12-07 13:41:43 +0300

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,24 +1,27 @@
 """UNIX cat command. Do the next things:
 1. Display the contents of a file(s)      - cat filename1 filename2 ...
 """
+import sys
 
 
-def cat(f_arg, *args):
+def cat(*args):
     """Display files' contents one by one."""
-    if f_arg == 'cat':
-        for arg in args:
+    for arg in args:
+        try:
             with open(arg, 'r+') as file_cont:
                 print file_cont.read()
-
-    else:
-        print "Usage: cat <filename1> <filename2> ..."
+        except IOError as err:
+            print err
 
 
 def main():
     """Test cat() function."""
-    cat('tac')
-    cat('cat', 'links.txt')
-    cat('cat', 'links.txt', 'another.py')
+    args = sys.argv[1:]
+    if not args:
+        print "Usage: cat.py <filename1> <filename2> ..."
+        sys.exit(1)
+
+    cat(*args)
 
 
 if __name__ == '__main__':
